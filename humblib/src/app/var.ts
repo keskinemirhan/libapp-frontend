11c4dae2dcac49d721c0b7fe00100da4118d9045
bam <keskinemirhan@gmail.com>
2023-03-24 22:32:26 +0300

    Add more global URL variables

--- humblib/src/app/var.ts
+++ humblib/src/app/var.ts
@@ -1,1 +1,7 @@
 export const BASE_URL: string = 'http://localhost:3000';
+export const BOOK_URL: string = BASE_URL + '/library/book';
+export const CAT_URL: string = BASE_URL + '/library/category';
+export const LIB_URL: string = BASE_URL + '/library';
+export const USER_URL: string = BASE_URL + '/users';
+export const PROF_URL: string = BASE_URL + '/users/profile';
+export const LOG_URL: string = BASE_URL + '/users/login';
