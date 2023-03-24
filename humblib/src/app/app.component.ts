040ad757bf207144235445ac610995ecde729c2d
bam <keskinemirhan@gmail.com>
2023-03-25 00:59:48 +0300

    Add comma

--- humblib/src/app/app.component.ts
+++ humblib/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoggerService } from './logger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'humblib';
