57259f11018b12ae0c3c60fb49742a53704e031c
bam <keskinemirhan@gmail.com>
2023-03-24 22:48:52 +0300

    Add create and get methods for book

--- humblib/src/app/library.service.ts
+++ humblib/src/app/library.service.ts
@@ -1,20 +1,43 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BASE_URL } from './var';
+import { LoggerService } from './logger.service';
+import { BASE_URL, BOOK_URL } from './var';
 
 @Injectable()
 export class LibraryService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private loggerService: LoggerService) {}
 
   getBooks(id: number) {
-    return this.http.get(BASE_URL + '/library/books');
+    return this.http
+      .get(BOOK_URL, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${this.loggerService.token}`,
+        },
+      })
+      .subscribe((data: any) => {
+        return data;
+      });
   }
 
-  getBook() {}
+  //   getBook() {}
 
-  patchBook() {}
+  //   patchBook() {}
 
-  deleteBook() {}
+  //   deleteBook() {}
 
-  createBook() {}
+  createBook(name: string, categories: Array<string>) {
+    return this.http
+      .post(BOOK_URL, {
+        name,
+        categories: [],
+      })
+      .subscribe((data: any) =>
+        this.http.patch(BOOK_URL, {
+          bookId: data.id,
+          bookName: name,
+          categories,
+        })
+      );
+  }
 }
