7eebebe2f686da21585530dd1f895373f8fb2f2d
bam <keskinemirhan@gmail.com>
2023-03-26 04:54:38 +0300

    Add category creating

--- humblib/src/app/categorization/categorization.component.ts
+++ humblib/src/app/categorization/categorization.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LibraryService } from '../library.service';
 
 @Component({
   selector: 'app-categorization',
   templateUrl: './categorization.component.html',
-  styleUrls: ['./categorization.component.css']
+  styleUrls: ['./categorization.component.css'],
 })
-export class CategorizationComponent {
+export class CategorizationComponent implements OnInit {
+  form: FormGroup;
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    public libraryService: LibraryService
+  ) {
+    this.form = this.fb.group({
+      name: [''],
+    });
+  }
+  onSubmit() {
+    this.libraryService.createCategory(this.form.value.name, '');
+    this.router
+      .navigateByUrl('/', { skipLocationChange: true })
+      .then(() => this.router.navigateByUrl('/categorization'));
+  }
 
+  ngOnInit(): void {
+    this.libraryService.getCategories();
+  }
 }
