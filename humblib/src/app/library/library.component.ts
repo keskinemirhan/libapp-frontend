cb06928ed953cffdffac4d13b224d0b242e38ff4
bam <keskinemirhan@gmail.com>
2023-03-26 16:25:22 +0300

    Add category control and ngOnInit  function

--- humblib/src/app/library/library.component.ts
+++ humblib/src/app/library/library.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LibraryService } from '../library.service';
 
 @Component({
   selector: 'app-library',
   templateUrl: './library.component.html',
-  styleUrls: ['./library.component.css']
+  styleUrls: ['./library.component.css'],
 })
-export class LibraryComponent {
+export class LibraryComponent implements OnInit {
+  constructor(public libraryService: LibraryService) {}
 
+  isCat(book: any, cat: any) {
+    return book.categories.some((e: any) => e.name === cat.name);
+  }
+  ngOnInit(): void {
+    this.libraryService.getCategories();
+    this.libraryService.getBooks();
+  }
 }
