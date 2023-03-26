a566e290fccf4ab8956c797705540839cf213888
bam <keskinemirhan@gmail.com>
2023-03-26 16:17:55 +0300

    Add auto login if  token exists on localStorage

--- humblib/src/app/logger.service.ts
+++ humblib/src/app/logger.service.ts
@@ -1,13 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { LOG_URL, PROF_URL, USER_URL } from './var';
 @Injectable()
 export class LoggerService {
-  constructor(private http: HttpClient) {}
   isLogged: boolean = false;
   token: string = '';
   profileName = '';
   feedBack = '';
+
+  constructor(private http: HttpClient, private router: Router) {
+    if (localStorage.getItem('humblibToken')) {
+      this.isLogged = true;
+      this.token = localStorage.getItem('token') as string;
+      this.getProfile(this.token).subscribe((data: any) => {
+        this.profileName = data.username;
+      });
+    }
+  }
 
   login(email: string, password: string) {
     return this.http.post(LOG_URL, {
