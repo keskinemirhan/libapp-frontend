fe5089907b9fcc3881c40f05aef109c1ca584050
bam <keskinemirhan@gmail.com>
2023-04-02 02:03:50 +0300

    Add token interceptor

--- humblib/src/app/core/core.module.ts
+++ humblib/src/app/core/core.module.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { HttpTokenInterceptor } from './interceptors';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true },
+  ],
 })
 export class CoreModule {}
