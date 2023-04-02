6c331aeb09425b6d90276cb71ee4bda182c49e1c
bam <keskinemirhan@gmail.com>
2023-04-02 05:31:19 +0300

    Refactor library service

--- humblib/src/app/core/services/library.service.ts
+++ humblib/src/app/core/services/library.service.ts
@@ -3,188 +3,199 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { LoggerService } from './logger.service';
 import { BOOK_URL, CAT_FLAT_URL, CAT_URL, NOTES_URL } from './var';
+import { ApiService } from './api.service';
 
 @Injectable({ providedIn: 'root' })
 export class LibraryService {
-  constructor(private http: HttpClient, private loggerService: LoggerService) {}
+  constructor(
+    private apiService: ApiService,
+    private http: HttpClient,
+    private loggerService: LoggerService
+  ) {}
   books: Array<any> = [];
   categories_nested: any;
   categories: Array<any> = [];
   subCategories = new BehaviorSubject(0);
 
   //=================== BOOKS ==================
-  getBooks() {
-    return this.http
-      .get(BOOK_URL, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${this.loggerService.token}`,
-        },
-      })
-      .subscribe((data: any) => {
-        this.books = data;
-      });
+
+  getBooks$() {
+    return this.apiService.get$(BOOK_URL);
   }
 
   //   getBook() {}
 
-  patchBook(bookId: number, bookName: string, categories: string[]) {
-    return this.http
-      .patch(
-        BOOK_URL,
-        {
-          bookId,
-          bookName,
-          categories,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${this.loggerService.token}`,
-          },
-        }
-      )
-      .subscribe();
+  patchBook$(body: any) {
+    return this.apiService.patch$(BOOK_URL, body);
   }
 
-  deleteBook(id: number) {
-    return this.http
-      .delete(BOOK_URL + `/${id}`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${this.loggerService.token}`,
-        },
-      })
-      .subscribe();
+  deleteBook$(id: number) {
+    return this.apiService.delete$(BOOK_URL);
   }
 
-  createBook(name: string, categories: Array<string>) {
-    return this.http
-      .post(
-        BOOK_URL,
-        {
-          name,
-          categories: [],
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${this.loggerService.token}`,
-          },
-        }
-      )
-      .subscribe((data: any) => {
-        this.http
-          .patch(
-            BOOK_URL,
-            {
-              bookId: data.id,
-              bookName: name,
-              categories: categories,
-            },
-            {
-              headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${this.loggerService.token}`,
-              },
-            }
-          )
-          .subscribe();
-      });
+  //temporary implementation
+  createBook$(body: any) {
+    return this.apiService
+      .post$(BOOK_URL, body)
+      .pipe((data: any) =>
+        this.apiService.patch$(BOOK_URL, { bookId: data.bookId, ...body })
+      );
   }
+  // createBook(name: string, categories: Array<string>) {
+  //   return this.http
+  //     .post(
+  //       BOOK_URL,
+  //       {
+  //         name,
+  //         categories: [],
+  //       },
+  //       {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           Authorization: `Bearer ${this.loggerService.token}`,
+  //         },
+  //       }
+  //     )
+  //     .subscribe((data: any) => {
+  //       this.http
+  //         .patch(
+  //           BOOK_URL,
+  //           {
+  //             bookId: data.id,
+  //             bookName: name,
+  //             categories: categories,
+  //           },
+  //           {
+  //             headers: {
+  //               'Content-Type': 'application/json',
+  //               Authorization: `Bearer ${this.loggerService.token}`,
+  //             },
+  //           }
+  //         )
+  //         .subscribe();
+  //     });
+  // }
 
   //=================== CATEGORIES ==================
 
-  getCategoriesNested() {
-    return this.http
-      .get(CAT_URL, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${this.loggerService.token}`,
-        },
-      })
-      .subscribe((data: any) => (this.categories_nested = data));
+  getCategoriesNested$() {
+    return this.apiService.get$(CAT_URL);
   }
 
-  getCategory(id: number) {
-    return this.http
-      .get(CAT_URL + `/${id}`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${this.loggerService.token}`,
-        },
-      })
-      .subscribe();
+  // getCategoriesNested() {
+  //   return this.http
+  //     .get(CAT_URL, {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Authorization: `Bearer ${this.loggerService.token}`,
+  //       },
+  //     })
+  //     .subscribe((data: any) => (this.categories_nested = data));
+  // }
+
+  getCategoriesArray$() {
+    return this.apiService.get$(CAT_FLAT_URL);
   }
 
-  getCategoriesArray() {
-    return this.http
-      .get(CAT_FLAT_URL, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${this.loggerService.token}`,
-        },
-      })
-      .subscribe((data: any) => (this.categories = data));
+  // getCategoriesArray() {
+  //   return this.http
+  //     .get(CAT_FLAT_URL, {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Authorization: `Bearer ${this.loggerService.token}`,
+  //       },
+  //     })
+  //     .subscribe((data: any) => (this.categories = data));
+  // }
+
+  getCategory$() {
+    return this.apiService.get$(CAT_URL);
+  }
+  // getCategory(id: number) {
+  //   return this.http
+  //     .get(CAT_URL + `/${id}`, {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         Authorization: `Bearer ${this.loggerService.token}`,
+  //       },
+  //     })
+  //     .subscribe();
+  // }
+
+  deleteCategory$(id: number) {
+    return this.apiService.delete$(CAT_URL + `/${id}`);
   }
 
-  deleteCategory(id: number) {
-    this.http.delete(CAT_URL + `/${id}`, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${this.loggerService.token}`,
-      },
-    });
+  // deleteCategory(id: number) {
+  //   this.http.delete(CAT_URL + `/${id}`, {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Authorization: `Bearer ${this.loggerService.token}`,
+  //     },
+  //   });
+  // }
+
+  createCategory$(body: any) {
+    this.apiService.post$(CAT_URL, body);
   }
 
-  createCategory(name: string, topCategory: string) {
-    this.http
-      .post(
-        CAT_URL,
-        {
-          name,
-          topCategory,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${this.loggerService.token}`,
-          },
-        }
-      )
-      .subscribe();
-  }
+  // createCategory(name: string, topCategory: string) {
+  //   this.http
+  //     .post(
+  //       CAT_URL,
+  //       {
+  //         name,
+  //         topCategory,
+  //       },
+  //       {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           Authorization: `Bearer ${this.loggerService.token}`,
+  //         },
+  //       }
+  //     )
+  //     .subscribe();
+  // }
+
   //======================================================
 
   //======================= NOTES ========================
 
-  getAllNotes() {
-    return this.http.get(NOTES_URL, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${this.loggerService.token}`,
-      },
-    });
+  getAllNotes$() {
+    return this.apiService.get$(NOTES_URL);
   }
 
-  createNote(title: string, note: string, bookId: number) {
-    return this.http
-      .post(
-        NOTES_URL,
-        {
-          bookId,
-          title,
-          note,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${this.loggerService.token}`,
-          },
-        }
-      )
-      .subscribe();
+  // getAllNotes() {
+  //   return this.http.get(NOTES_URL, {
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       Authorization: `Bearer ${this.loggerService.token}`,
+  //     },
+  //   });
+  // }
+
+  createNote$(body: any) {
+    return this.apiService.post$(NOTES_URL, body);
   }
+
+  // createNote(title: string, note: string, bookId: number) {
+  //   return this.http
+  //     .post(
+  //       NOTES_URL,
+  //       {
+  //         bookId,
+  //         title,
+  //         note,
+  //       },
+  //       {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           Authorization: `Bearer ${this.loggerService.token}`,
+  //         },
+  //       }
+  //     )
+  //     .subscribe();
+  // }
 
   //======================================================
 }
