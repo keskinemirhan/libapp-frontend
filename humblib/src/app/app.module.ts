09500c7c1955affe3484db4e026bec7ecd088140
cppmonger <keskinemirhan@gmail.com>
2023-04-09 23:23:17 +0300

    Add angular material and customize

--- humblib/src/app/app.module.ts
+++ humblib/src/app/app.module.ts
@@ -21,6 +21,10 @@ import { NotesComponent } from './notes/notes.component';
 import { AddnoteComponent } from './addnote/addnote.component';
 import { BooknotesComponent } from './booknotes/booknotes.component';
 import { CoreModule } from './core/core.module';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -50,6 +54,9 @@ import { CoreModule } from './core/core.module';
     FormsModule,
     BrowserAnimationsModule,
     CoreModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
