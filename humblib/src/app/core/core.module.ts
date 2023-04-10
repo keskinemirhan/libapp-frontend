16749b12f743895df0af0899825ba51a227116e1
cppmonger <keskinemirhan@gmail.com>
2023-04-11 01:38:41 +0300

    Add line pipe to display new line characters of notes

--- humblib/src/app/core/core.module.ts
+++ humblib/src/app/core/core.module.ts
@@ -2,11 +2,14 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { HttpTokenInterceptor } from './interceptors';
+import { LinePipe } from './pipes';
 
 @NgModule({
   imports: [CommonModule, HttpClientModule],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true },
   ],
+  declarations: [LinePipe],
+  exports: [LinePipe],
 })
 export class CoreModule {}
