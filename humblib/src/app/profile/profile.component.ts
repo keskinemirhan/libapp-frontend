fc633dc2d28716f4022812cf012da85e87a64780
cppmonger <keskinemirhan@gmail.com>
2023-04-12 17:22:35 +0300

    Add logout

--- humblib/src/app/profile/profile.component.ts
+++ humblib/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { LoggerService } from '../core/services/logger.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,11 +8,18 @@ import { LoggerService } from '../core/services/logger.service';
   styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-  constructor(public loggerService: LoggerService) {}
+  constructor(public loggerService: LoggerService, private router: Router) {}
   profile = '';
+  isLogged = false;
+
+  logout() {
+    this.loggerService.deleteLogInfo();
+    this.router.navigateByUrl('/login');
+  }
 
   ngOnInit(): void {
     this.loggerService.isLogged.asObservable().subscribe((data: boolean) => {
+      this.isLogged = data;
       this.loggerService.profileName.asObservable().subscribe((data: any) => {
         if (data) this.profile = data;
         else this.profile = 'Login';
