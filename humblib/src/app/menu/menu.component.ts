bf5a8631e64535da3cbcc67d1bb9f12a28ba6ac1
cppmonger <keskinemirhan@gmail.com>
2023-04-12 18:31:14 +0300

    Delete unnecessary clickList() guard

--- humblib/src/app/menu/menu.component.ts
+++ humblib/src/app/menu/menu.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { LoggerService } from '../core';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css'],
 })
-export class MenuComponent {
-  constructor(private loggerService: LoggerService, private router: Router) {}
-  clickList() {
-    if (!this.loggerService.isLogged.getValue())
-      this.router.navigate(['/login']);
-  }
-}
+export class MenuComponent {}
